b1c5271541827424b49b3f59f5b8ce71b5aa785c
Guilherme Faccini <faccini.g@gmail.com>
2017-07-24 21:00:06 -0300

    re_locate

---
+++ R/re_locate.R
@@ -0,0 +1,37 @@
+
+
+re_locate_all <- function(x, pattern) {
+  .simplify_match <- function(.m) {
+    if (is.na(.m)) {
+      start <- NA_integer_
+      end <- NA_integer_
+    } else if (.m > 0L) {
+      start <- as.integer(.m)
+      end <- attr(.m,"match.length") + start - 1L
+    } else {
+      start <- -1L
+      end <- -1L
+    }
+    cbind(
+      start,
+      end
+    )
+  }
+  .gregexpr <- function(x, pattern) {
+    if (is.na(pattern)) return(NA)
+    gregexpr(pattern, x,useBytes = FALSE,perl = TRUE)
+  }
+  x <- if_na(x, "")
+  if (length(pattern) > 1) {
+    res <- map_lst(.gregexpr, x, pattern)
+    res <- map_lst(`[[`,res,1L)
+  } else res <- .gregexpr(x, pattern)
+  map_lst(.simplify_match, res)
+}
+
+re_locate <- function(x, pattern) {
+  res <- re_locate_all(x, pattern)
+  res <- map_lst(function(.x) .x[1,],res)
+  do.call(rbind, res)
+}
+
